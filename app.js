64667137c897542002d3903a171159d2bb838784
subinggrae <subin000602@gmail.com>
2024-09-05 18:28:10 +0900

    feature: http 메소드 추가(put, delete)
    포켓몬 도감의 내용을 수정하는 기능,
    포켓몬 도감에서 특정 번호의 포켓몬을 삭제하는 기능을 추가했음

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ db.set(1, { no: 1, name: '이상해씨', type: ['풀', '독'], height: 0.7, weig
 db.set(2, { no: 2, name: '이상해풀', type: ['풀', '독'], height: 1.0, weight: 13.0 });
 db.set(3, { no: 3, name: '이상해꽃', type: ['풀', '독'], height: 2.0, weight: 100.0 });
 
-app.get('/poketmons/:no', function(req, res) {
+app.get('/poketmons', function (req, res) {
+  let poketmons = {};
+  db.forEach((value) => {
+    poketmons[value.no] = value;
+  })
+
+  res.json(poketmons);
+})
+
+app.get('/poketmons/:no', function (req, res) {
   let { no } = req.params;
   no = parseInt(no);
   
@@ -23,7 +32,7 @@ app.get('/poketmons/:no', function(req, res) {
   }
 })
 
-app.post('/poketmons', function(req, res) {
+app.post('/poketmons', function (req, res) {
   let { no, name } = req.body;
 
   if (no == undefined) {
@@ -34,6 +43,55 @@ app.post('/poketmons', function(req, res) {
     db.set(no, req.body);
     res.json({
       message: `도감번호 ${no}번 포켓몬 ${name}(이)가 등록되었습니다.`
+    })
+  }
+})
+
+app.put('/poketmons/:no', function (req, res) {
+  let { no } = req.params;
+  no = parseInt(no);
+
+  if (db.get(no) == undefined) {
+    res.json({
+      message: `${no}번은 도감에 등록되지 않은 번호입니다.`
+    })
+  } else {
+    db.set(no, req.body);
+
+    res.json({
+      message: `${no}번 포켓몬 정보가 수정되었습니다.`
+    })
+  }
+})
+
+app.delete('/poketmons', function (req, res) {
+
+  if (db.size == 0) {
+    res.json({
+      message: '도감에 등록된 포켓몬이 하나도 없습니다.'
+    }) 
+  } else {
+    db.clear();
+    res.json({
+      message: '도감이 초기화되었습니다.'
+    })
+  }
+})
+
+app.delete('/poketmons/:no', function (req, res) {
+  let { no } = req.params;
+  no = parseInt(no);
+
+  if (db.get(no) == undefined) {
+    res.json({
+      message: `${no}번은 도감에 등록되지 않은 번호입니다.`
+    })
+  } else {
+    let { name }  = db.get(no) 
+
+    db.delete(no);
+    res.json({
+      message: `${name}(이)가 도감에서 삭제되었습니다.`
     })
   }
 })
